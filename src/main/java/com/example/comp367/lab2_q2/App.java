dcd0a72cb0ffbc3cb68802113e5a1cde1aba6e3a
ycheun29 <ycheun29@my.centennialcollege.ca>
2022-10-24 17:14:16 -0400

    first commit

---
+++ src/main/java/com/example/comp367/lab2_q2/App.java
@@ -0,0 +1,13 @@
+package com.example.comp367.lab2_q2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to COMP367" );
+    }
+}
